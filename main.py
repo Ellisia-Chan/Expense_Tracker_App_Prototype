6c67ca6290917388ff3e91172de8c31e9780ac28
Ren <152279955+ChristianJude23@users.noreply.github.com>
2024-04-28 22:43:39 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import ttk
+from datetime import datetime, timedelta
 
 
 class StartUp(tk.Tk):
@@ -53,6 +55,8 @@ class MyApp(tk.Tk):
 
         self.create_widgets()
 
+        AppFunction(self)
+
     def create_widgets(self):
         # Frames
         self.top_frame = tk.Frame(self, width=500, height=180, bg="#102C57")
@@ -64,54 +68,92 @@ class MyApp(tk.Tk):
         self.mid_frame.place(x=0, y=180)
         self.bot_frame.place(x=0, y=670)
 
+        # Date
+        self.current_date = tk.StringVar()
+        self.current_date.set(datetime.now().strftime("%B %d, %Y"))
+
         # Top Frame widgets
         # Lbl
-        lbl_date = tk.Label(self.top_frame, text="Date Here", font=("katibeh", 20, "bold"), bg="#102C57", fg="#fff")
+        self.lbl_date = tk.Label(self.top_frame, textvariable=self.current_date, font=("katibeh", 20, "bold"), bg="#102C57",
+                                 fg="#fff")
         lbl_expenses = tk.Label(self.top_frame, text="Expenses", font=("katibeh", 14, "bold"), bg="#102C57", fg="#fff")
         lbl_income = tk.Label(self.top_frame, text="Income", font=("katibeh", 14, "bold"), bg="#102C57", fg="#fff")
         lbl_balance = tk.Label(self.top_frame, text="Balance", font=("katibeh", 14, "bold"), bg="#102C57", fg="#fff")
 
-        lbl_expenses_amount = tk.Label(self.top_frame, text="₱1,000,000", font=("katibeh", 14, "bold"), bg="#102C57",
-                                       fg="#FF0000")
-        lbl_income_amount = tk.Label(self.top_frame, text="₱1,000,000", font=("katibeh", 14, "bold"), bg="#102C57",
-                                     fg="#90EE90")
-        lbl_amount_amount = tk.Label(self.top_frame, text="₱1,000,000", font=("katibeh", 14, "bold"), bg="#102C57",
-                                     fg="#fff")
+        self.lbl_expenses_amount = tk.Label(self.top_frame, text="₱1,000,000", font=("katibeh", 14, "bold"),
+                                            bg="#102C57",
+                                            fg="#FF0000")
+        self.lbl_income_amount = tk.Label(self.top_frame, text="₱1,000,000", font=("katibeh", 14, "bold"), bg="#102C57",
+                                          fg="#90EE90")
+        self.lbl_amount_amount = tk.Label(self.top_frame, text="₱1,000,000", font=("katibeh", 14, "bold"), bg="#102C57",
+                                          fg="#fff")
 
         # Btn
         btn_menu = tk.Button(self.top_frame, text="≡", font=("katibeh", 14, "bold"), bg="#102C57", fg="#fff", width=3)
         btn_search = tk.Button(self.top_frame, text="🔍", font=("katibeh", 14, "bold"), bg="#102C57", fg="#fff", width=3)
 
         btn_date_next = tk.Button(self.top_frame, text=">", font=("katibeh", 14, "bold"), bg="#102C57", fg="#fff",
-                                  width=3)
+                                  width=3, command=AppFunction(self).next_date)
         btn_date_previous = tk.Button(self.top_frame, text="<", font=("katibeh", 14, "bold"), bg="#102C57", fg="#fff",
-                                      width=3)
+                                      width=3, command=AppFunction(self).previous_date)
 
         # Top Frame Widgets pos
         # lbl
-        lbl_date.place(x=185, y=50)
+        self.lbl_date.place(x=160, y=50)
         lbl_expenses.place(x=40, y=110)
         lbl_income.place(x=210, y=110)
         lbl_balance.place(x=360, y=110)
 
-        lbl_expenses_amount.place(x=30, y=140)
-        lbl_income_amount.place(x=200, y=140)
-        lbl_amount_amount.place(x=350, y=140)
+        self.lbl_expenses_amount.place(x=30, y=140)
+        self.lbl_income_amount.place(x=200, y=140)
+        self.lbl_amount_amount.place(x=350, y=140)
 
         # Btn
         btn_menu.place(x=10, y=10)
         btn_search.place(x=450, y=10)
 
-        btn_date_next.place(x=340, y=50)
-        btn_date_previous.place(x=120, y=50)
+        btn_date_next.place(x=360, y=50)
+        btn_date_previous.place(x=100, y=50)
 
         # Mid Frame widgets
-        lb_listbox = tk.Listbox(self.mid_frame, width=44, height=30, borderwidth=0, highlightthickness=0,
-                                font=("katibeh", 14, "bold"), bg="#EADBC8")
+        # TreeView
+        self.tv_tree_view = ttk.Treeview(self.mid_frame, columns=(1, 2), show="headings", height=23)
+        self.tv_tree_view.column(1, anchor="center", stretch="No", width=240)
+        self.tv_tree_view.column(2, anchor="center", stretch="No", width=240)
+        self.tv_tree_view.heading(1, text="Name")
+        self.tv_tree_view.heading(2, text="Amount")
 
-        # Mid Frame Widgets pos
-        lb_listbox.place(x=10, y=10)
+        # Mid Frame Pos
+        self.tv_tree_view.pack(side="left")
 
+        # Tree View Style
+        style = ttk.Style()
+        style.theme_use("clam")
+        style.configure("Treeview.Heading", font=("Helvetica", 18))
+        style.configure("Treeview", font=("Helvetica", 14), background="#EADBC8", fieldbackground="#EADBC8")
+
+        self.tv_tree_view.bind('<Button-1>', 'break')
+
+        scrollbar = ttk.Scrollbar(self.mid_frame, orient="vertical")
+        scrollbar.configure(command=self.tv_tree_view.yview())
+        scrollbar.pack(side="right", fill="y")
+
+class AppFunction:
+    def __init__(self, my_app):
+        self.win_widgets = my_app
+        self.current_date = my_app.current_date
+
+    def previous_date(self):
+        current = datetime.strptime(self.current_date.get(), "%B %d, %Y")
+        new_date = current - timedelta(days=1)
+        self.current_date.set(new_date.strftime("%B %d, %Y"))
+
+    def next_date(self):
+        current = datetime.strptime(self.current_date.get(), "%B %d, %Y")
+        new_date = current + timedelta(days=1)
+        self.current_date.set(new_date.strftime("%B %d, %Y"))
+     
+    
 
 # Call StartUp
 # app = StartUp()
